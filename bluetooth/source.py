6bd89560931761720a56195e986c31fd47b57895
goajos <jappecremers@gmail.com>
2025-11-19 21:28:23 +0100

    bluetooth module with main.conf update

--- bluetooth/source.py
+++ bluetooth/source.py
@@ -1,8 +1,20 @@
-import decman
+from decman import File, Module
 
-decman.packages += [
-    "bluez",  # Daemons for the bluetooth protocol stack
-    "bluez-utils",  # Development and debugging utilities for the bluetooth protocol stack
-]
 
-decman.enabled_systemd_units += ["bluetooth.service"]
+class Bluetooth(Module):
+    def __init__(self):
+        super().__init__(name="bluetooth", enabled=True, version="1")
+
+    def files(self) -> dict[str, File]:
+        return {
+            "/etc/bluetooth/main.conf": File(source_file="bluetooth/main.conf"),
+        }
+
+    def pacman_packages(self) -> list[str]:
+        return [
+            "bluez",  # Daemons for the bluetooth protocol stack
+            "bluez-utils",  # Development and debugging utilities for the bluetooth protocol stack
+        ]
+
+    def systemd_units(self) -> list[str]:
+        return ["bluetooth.service"]
