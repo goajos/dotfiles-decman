d42a8bac67280ca6aae601ef66749afab193ebfe
goajos <jappecremers@gmail.com>
2025-11-19 22:16:28 +0100

    required fonts setup

--- fonts/source.py
+++ fonts/source.py
@@ -1,14 +1,22 @@
-from decman import File, Module
+from decman import Directory, File, Module
 
 
+# TODO: set the gnome fonts here or in Gnome module?
 class Fonts(Module):
     def __init__(self):
         super().__init__(name="fonts", enabled=True, version="1")
 
-    # TODO: what to do with installing the fonts?
     def files(self) -> dict[str, File]:
         return {
             "/home/jappe/.config/fontconfig/fonts.conf": File(
                 source_file="fonts/fonts.conf"
             ),
+        }
+
+    def directories(self) -> dict[str, Directory]:
+        return {
+            "/usr/local/share/fonts/ttf/JetBrainsMono": Directory(
+                source_directory="fonts/JetBrainsMono"
+            ),
+            "/usr/local/share/fonts/ttf/Noto": Directory(source_directory="fonts/Noto"),
         }
