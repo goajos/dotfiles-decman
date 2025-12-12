b612a091de2af4df3b9eaee0e4ecd0d230a33b6c
goajos <jappecremers@gmail.com>
2025-12-12 14:43:33 +0100

    moved the decman command into niri instead of waybar

--- waybar/source.py
+++ waybar/source.py
@@ -4,7 +4,6 @@ from decman import Directory, File, Module, sh
 from config import USER
 
 
-# TODO: clickable waybar module to run decman? => move this to a niri keybind?
 class Waybar(Module):
     def __init__(self):
         super().__init__(name="waybar", enabled=True, version="1")
